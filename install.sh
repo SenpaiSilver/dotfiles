4a19a66047d881463809586a9d27f0b55493f337
SenpaiSilver <silvermesprims@gmail.com>
2016-01-26 15:55:48 +0100

    Install script

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+echo Checking for curl
 if ! which curl > /dev/null; then
+	echo You need curl
 	sudo apt-get install curl -y
 fi
 
+echo Getting Plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+echo Updating Plugs
 vim -c ":PlugUpdate" -c ":q" -c ":q"
-
-for i in ./home/*; do
-	cp $i ~/ -Rv
-done
+echo Installing files
+cp ./home/.vim/ \
+	./home/.vimrc \
+	./home/.bashrc \
+	./home/.gitconfig \
+	~/ -Rv
+echo Done
